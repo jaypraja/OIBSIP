dff7abc46d7a2afb273e273204e05b7ccb1df79f
jaypraja <123640100+jaypraja@users.noreply.github.com>
2023-01-26 19:05:51 +0530

    Task 2

---
+++ NumberGuessingGame/src/numberguessinggame/NumberGuessingGame.java
@@ -0,0 +1,14 @@
+
+package numberguessinggame;
+
+
+public class NumberGuessingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
